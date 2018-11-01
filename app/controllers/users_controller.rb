f880bcf6e3891b266bd9d2e1cb9dcdf2ed74a9c8
Al Barleta <al.barleta@gmail.com>
2018-11-01 21:49:06 +0000

    Comlplete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, except: [:index, :show]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -31,7 +33,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user_articles = @user.articles.paginate(page: [:page], per_page: 5)
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
     
     def set_user
       @user = User.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-  root 'pages#home'
-  get  '/about', to: "pages#about"
+  get    'sessions/new'
+  get    'articles/new'
+  root   'pages#home'
+  get    '/about',  to: "pages#about"
   resources :articles
-  get '/signup', to: 'users#new'
+  get    '/signup', to: 'users#new'
   resources :users, except: [:new]
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
