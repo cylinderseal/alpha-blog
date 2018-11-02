5df213861cf62a8a0bed5fa4eb6b718b55d9d7a4
Al Barleta <al.barleta@gmail.com>
2018-11-02 11:17:25 +0000

    Add styles for homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:index, :show]
   before_action :require_same_user, except: [:index, :show]
   
   def index
